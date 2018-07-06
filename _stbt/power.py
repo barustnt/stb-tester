a15674589780ebce6bbe24d0e4d666e3ae822048
William Manley <will@stb-tester.com>
2018-07-06 17:25:22 +0100

    SNMP power: Allow specifying port in address
    You can't actually do this through the factory as `:` is our separator,
    but it could help when constructing these directly.

--- _stbt/power.py
+++ _stbt/power.py
@@ -10,7 +10,7 @@ def uri_to_power_outlet(uri):
     remotes = [
         (r'none', _NoOutlet),
         (r'file:(?P<filename>[^:]+)', _FileOutlet),
-        (r'aten:(?P<hostname>[^: ]+):(?P<outlet>[^: ]+)', _ATEN_PE6108G),
+        (r'aten:(?P<address>[^: ]+):(?P<outlet>[^: ]+)', _ATEN_PE6108G),
         (r'(?P<model>pdu|ipp|testfallback):(?P<hostname>[^: ]+)'
          ':(?P<outlet>[^: ]+)', _ShellOutlet),
         (r'aviosys-8800-pro(:(?P<filename>[^:]+))?', _new_aviosys_8800_pro),
@@ -196,11 +196,11 @@ class _FakeAviosys8800ProSerial(object):
 class _ATEN_PE6108G(object):
     """Class to control the ATEN PDU using pysnmp module. """
 
-    def __init__(self, hostname, outlet):
-        outlet_offset = 1 if self.outlet <= 8 else 2
+    def __init__(self, address, outlet):
+        outlet_offset = 1 if outlet <= 8 else 2
         self._snmp = _SnmpInteger(
-            hostname, "1.3.6.1.4.1.21317.1.3.2.2.2.2.%i.0" % (
-                self.outlet + outlet_offset),
+            address, "1.3.6.1.4.1.21317.1.3.2.2.2.2.%i.0" % (
+                outlet + outlet_offset),
             community='administrator')
 
     def set(self, power):
@@ -222,11 +222,15 @@ class _ATEN_PE6108G(object):
 
 
 class _SnmpInteger(object):
-    def __init__(self, hostname, oid, community):
+    def __init__(self, address, oid, community):
         import pysnmp.hlapi as snmp
         self.oid = oid
         self._community = community
-        self._transport = snmp.UdpTransportTarget((hostname, 161))
+        if ':' in address:
+            address, port = address.split(address, 2)
+        else:
+            port = "161"
+        self._transport = snmp.UdpTransportTarget((address, int(port)))
 
     def set(self, value):
         return self._cmd(value)
